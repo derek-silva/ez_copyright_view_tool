4e3523264c161eb91e9c121ba5d60494054c65b0
Derek Silva <dereksilva.tech@gmail.com>
2019-01-30 00:13:39 -0600

    Dev tool initial commit.

---
+++ lib/ez_copyright_view_tool.rb
@@ -0,0 +1,7 @@
+require "ez_copyright_view_tool/version"
+require "ez_copyright_view_tool/renderer"
+
+module EzCopyrightViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
